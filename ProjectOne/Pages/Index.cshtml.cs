9de242d2b4f13724a4596fc62a5a4fc5c41e7a38
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-02-16 08:07:10 +0530

    Commit details: 1. New razor page added with title 'Intro'. 2. Link to intro page added in _layout.

---
+++ ProjectOne/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectOne.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string introMessage { get; set; } = "";
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ProjectOne/Pages/Intro.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectOne.Pages
+{
+    public class IntroModel : PageModel
+    {
+        private readonly ILogger<IntroModel> _logger;
+
+        public IntroModel(ILogger<IntroModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
